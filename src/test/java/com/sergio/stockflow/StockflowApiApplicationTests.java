9430b432ebd44c14060f420d1d6bc80120d7f213
DESKTOP-SE68HQN\Sergio <sergiomartinez26m@gmail.com>
2026-08-13 14:31:43 +0200

    first commit

---
+++ src/test/java/com/sergio/stockflow/StockflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.stockflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
